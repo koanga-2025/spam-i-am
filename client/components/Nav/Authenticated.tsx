c75e96adeb58b7089fd0760bcb95e6d01754361e
Nikola <227923415+NikolaBodor@users.noreply.github.com>
2025-11-19 11:56:31 +1300

    feat: add working login and logout buttons

--- client/components/Nav/Authenticated.tsx
+++ client/components/Nav/Authenticated.tsx
@@ -1,10 +1,8 @@
-// TODO: import useAuth0 function
 import { useAuth0 } from '@auth0/auth0-react'
 
 const useIsAuthenticated = () => {
   const { isAuthenticated } = useAuth0()
-  // TODO: call the useAuth0 hook, destructure and replace `return true`with return isAuthenticated
-  return true
+  return isAuthenticated
 }
 interface Props {
   children: React.ReactNode
--- client/components/Nav/LoginButton.tsx
+++ client/components/Nav/LoginButton.tsx
@@ -1,22 +1,18 @@
 import Button from '../UI/Button.tsx'
 import { IfAuthenticated, IfNotAuthenticated } from './Authenticated.tsx'
+import { useAuth0 } from '@auth0/auth0-react'
 
 function LoginButton() {
-  // TODO: call the useAuth0 hook and destructure user, logout, and loginWithRedirect
-
-  // TODO: replace placeholder user object with the one from auth0
-  const user = {
-    nickname: 'sarah.spamalot',
-  }
+  const { user, logout, loginWithRedirect } = useAuth0()
 
   const handleSignOut = () => {
     console.log('sign out')
-    //   TODO: return the logout function
+    return logout()
   }
 
   const handleSignIn = () => {
     console.log('sign in')
-    //   TODO: return the loginWithRedirect function
+    return loginWithRedirect()
   }
 
   return (
