b8b436368894ecde0bf82436190d5e1963159467
Nikola <227923415+NikolaBodor@users.noreply.github.com>
2025-11-19 16:16:04 +1300

    chore: removed unnecessary comments

--- client/__tests__/Auth.test.tsx
+++ client/__tests__/Auth.test.tsx
@@ -1,34 +1,16 @@
 // @vitest-environment jsdom
 import { renderApp } from '../test-setup.tsx'
-import {
-  describe,
-  it,
-  expect,
-  test,
-  beforeEach,
-  afterEach,
-  vi,
-  beforeAll,
-  afterAll,
-} from 'vitest'
-import App from '../App.tsx'
+import { describe, it, expect, beforeEach, afterEach, vi } from 'vitest'
 import { useAuth0 } from '@auth0/auth0-react'
-import nock from 'nock'
-import { render, screen } from '@testing-library/react'
 
 vi.mock('@auth0/auth0-react')
 
 const ACCESS_TOKEN = 'mock-access-token'
-const TEST_USER = { sub: 'bob@example.com' }
-
-// beforeAll(() => {
-//   nock.disableNetConnect()
-// })
 
 beforeEach(() => {
   vi.mocked(useAuth0).mockReturnValue({
     isAuthenticated: true,
-    user: TEST_USER,
+    user: { sub: 'bob@example.com', nickname: 'bob' },
     getAccessTokenSilently: vi.fn().mockReturnValue(ACCESS_TOKEN),
     loginWithRedirect: vi.fn(),
     logout: vi.fn(),
@@ -40,10 +22,10 @@ afterEach(() => {
 })
 
 describe('Auth.test.tsx', () => {
-  it('shows the correct name from auth0 users', async () => {
+  it('checks if app shows the correct name from auth0 users', async () => {
     const { ...screen } = renderApp('/')
-
-    const nickname = screen.getByText(`Signed in as: ${TEST_USER.nickname}`)
+    const testNickname = 'bob'
+    const nickname = screen.getByText(`Signed in as: ${testNickname}`)
 
     expect(nickname).toBeVisible()
   })
