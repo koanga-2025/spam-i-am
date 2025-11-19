d170822f0a3cf0a97d6e9c57d51ddebad07eaf8d
Jonell Balanay <itsjonellmb@gmail.com>
2025-11-19 16:10:22 +1300

    chore: use data text and image from hooks

--- client/Pages/About.tsx
+++ client/Pages/About.tsx
@@ -1,61 +1,11 @@
+import { useAboutText, useAboutImages } from '../hooks/useAbout'
 function About() {
-  // TODO: replace this fake data with real data from the dabatase
-  const spamHistory = [
-    {
-      title: 'Title I',
-      body: `SPAM ipsum dolor sit amet, delicious pork shoulder adipiscing elit. Nulla vel condimentum SPAM in, condimentum eget, lectus. Whether fried, baked, or straight from the can, SPAM makes every meal a delight. Ut enim ad minim veniam, quis nostrud SPAM exercitation laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in ham hock in voluptate velit esse cillum SPAM dolore eu fugiat nulla pariatur. A slice of SPAM is the best way to start the day. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. From breakfast scrambles to SPAM sandwiches, the possibilities are endless. Nullam quis risus eget urna mollis ornare vel SPAM lectus. Integer posuere erat a ante venenatis dapibus posuere velit SPAM aliquet. Crispy on the outside, tender on the inside, SPAM brings joy to your taste buds. Curabitur blandit tempus SPAM porttitor. Maecenas faucibus mollis interdum. Cum sociis natoque SPAM penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras mattis condimentum fermentum, SPAM condimentum purus.`,
-    },
-    {
-      title: 'Title II',
-      body: `SPAM ipsum dolor sit amet, delicious pork shoulder adipiscing elit. Nulla vel condimentum SPAM in, condimentum eget, lectus. Whether fried, baked, or straight from the can, SPAM makes every meal a delight. Ut enim ad minim veniam, quis nostrud SPAM exercitation laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in ham hock in voluptate velit esse cillum SPAM dolore eu fugiat nulla pariatur. A slice of SPAM is the best way to start the day. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. From breakfast scrambles to SPAM sandwiches, the possibilities are endless. Nullam quis risus eget urna mollis ornare vel SPAM lectus. Integer posuere erat a ante venenatis dapibus posuere velit SPAM aliquet. Crispy on the outside, tender on the inside, SPAM brings joy to your taste buds. Curabitur blandit tempus SPAM porttitor. Maecenas faucibus mollis interdum. Cum sociis natoque SPAM penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras mattis condimentum fermentum, SPAM condimentum purus.`,
-    },
-    {
-      title: 'Title III',
-      body: `SPAM ipsum dolor sit amet, delicious pork shoulder adipiscing elit. Nulla vel condimentum SPAM in, condimentum eget, lectus. Whether fried, baked, or straight from the can, SPAM makes every meal a delight. Ut enim ad minim veniam, quis nostrud SPAM exercitation laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in ham hock in voluptate velit esse cillum SPAM dolore eu fugiat nulla pariatur. A slice of SPAM is the best way to start the day. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. From breakfast scrambles to SPAM sandwiches, the possibilities are endless. Nullam quis risus eget urna mollis ornare vel SPAM lectus. Integer posuere erat a ante venenatis dapibus posuere velit SPAM aliquet. Crispy on the outside, tender on the inside, SPAM brings joy to your taste buds. Curabitur blandit tempus SPAM porttitor. Maecenas faucibus mollis interdum. Cum sociis natoque SPAM penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras mattis condimentum fermentum, SPAM condimentum purus.`,
-    },
-    {
-      title: 'Title IV',
-      body: `SPAM ipsum dolor sit amet, delicious pork shoulder adipiscing elit. Nulla vel condimentum SPAM in, condimentum eget, lectus. Whether fried, baked, or straight from the can, SPAM makes every meal a delight. Ut enim ad minim veniam, quis nostrud SPAM exercitation laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in ham hock in voluptate velit esse cillum SPAM dolore eu fugiat nulla pariatur. A slice of SPAM is the best way to start the day. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. From breakfast scrambles to SPAM sandwiches, the possibilities are endless. Nullam quis risus eget urna mollis ornare vel SPAM lectus. Integer posuere erat a ante venenatis dapibus posuere velit SPAM aliquet. Crispy on the outside, tender on the inside, SPAM brings joy to your taste buds. Curabitur blandit tempus SPAM porttitor. Maecenas faucibus mollis interdum. Cum sociis natoque SPAM penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras mattis condimentum fermentum, SPAM condimentum purus.`,
-    },
-    {
-      title: 'Epilogue',
-      body: `SPAM ipsum dolor sit amet, pork shoulder adipiscing elit. Nulla condimentum SPAM, condimentum eget lectus. Whether fried or baked, SPAM makes every meal a delight. From breakfast scrambles to SPAM sandwiches, the possibilities are endless. Crispy outside, tender inside, SPAM brings joy to every bite, a reminder of delicious, simple times.`,
-    },
-  ]
+  const { data: spamHistory } = useAboutText()
+  const { data: images } = useAboutImages()
 
-  const images = [
-    {
-      link: 'https://placehold.co/300x200/png',
-      alt: '',
-      caption:
-        'SPAM ipsum dolor sit amet, delicious pork shoulder adipiscing elit.',
-    },
-    {
-      link: 'https://placehold.co/300x200/png',
-      alt: '',
-      caption:
-        'SPAM ipsum dolor sit amet, delicious pork shoulder adipiscing elit.',
-    },
-    {
-      link: 'https://placehold.co/300x200/png',
-      alt: '',
-      caption:
-        'SPAM ipsum dolor sit amet, delicious pork shoulder adipiscing elit.',
-    },
-    {
-      link: 'https://placehold.co/300x200/png',
-      alt: '',
-      caption:
-        'SPAM ipsum dolor sit amet, delicious pork shoulder adipiscing elit.',
-    },
-    {
-      link: 'https://placehold.co/300x200/png',
-      alt: '',
-      caption:
-        'SPAM ipsum dolor sit amet, delicious pork shoulder adipiscing elit.',
-    },
-  ]
-
+  if (!spamHistory || !images) {
+    return <p>Loading...</p>
+  }
   return (
     // TODO: Style this page!
     <>
