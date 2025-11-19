7c7cfb0c935883c056170d83a52efa3f7e54b7ee
Matt <mattlocktom@gmail.com>
2025-11-19 17:04:00 +1300

    feat(comments): display comments on spam details page

--- client/hooks/useComments.ts
+++ client/hooks/useComments.ts
@@ -1,3 +1,13 @@
+import { useQuery } from '@tanstack/react-query'
+import { getAllCommentsBySpamId } from '../apis/comments'
+
 // TODO: Create custom hook for querying the comments by spamId
+
+export function useComments(spamId: number) {
+  return useQuery({
+    queryKey: ['comments', spamId],
+    queryFn: () => getAllCommentsBySpamId(spamId),
+  })
+}
 
 // TODO: Create custom hook for adding a new comment
