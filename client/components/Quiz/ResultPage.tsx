e05a15b7d66e3a6ce3c570ff7b580214a0f0930f
Jonell Balanay <itsjonellmb@gmail.com>
2025-11-21 15:54:46 +1300

    feat: fetch and display quiz results from API

--- client/components/Quiz/ResultPage.tsx
+++ client/components/Quiz/ResultPage.tsx
@@ -1,19 +1,56 @@
-import { QuizAnswers } from '../../../models/spam'
+import { useState, useEffect } from 'react'
+import { QuizAnswers, QuizResult } from '../../../models/spam'
+import calculateQuiz from '../../utils/calculateQuiz'
+import { getQuizResult } from '../../apis/quiz'
 
 interface Props {
   answers: QuizAnswers
 }
 
 function ResultPage({ answers }: Props) {
-  // TODO: Find and use some middleware from the 'client/utils' folder
-  // which will help you calculate the user's quiz result.
+  const [result, setResult] = useState<QuizResult | null>(null)
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState<string | null>(null)
 
-  // TODO: Replace the following hardcoded data with real results data from backend API endpoint
-  const result = {
-    id: 1,
-    name: 'Fake Spam Flavour ',
-    image: 'fake-spam.jpeg',
-    info: 'fake information about this spam flavour',
+  useEffect(() => {
+    const fetchResult = async () => {
+      try {
+        setLoading(true)
+        setError(null)
+
+        const category = calculateQuiz(answers)
+
+        const quizResult = await getQuizResult(category)
+        setResult(quizResult)
+      } catch (err) {
+        setError('Failed to load quiz results')
+        console.error('Error fetching quiz result:', err)
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    fetchResult()
+  }, [answers])
+
+  if (loading) {
+    return (
+      <section className="flex flex-col items-center justify-center p-5">
+        <div className="pt-20 font-heading text-heading-lg font-heading-bold text-spamBlue">
+          Loading your results...
+        </div>
+      </section>
+    )
+  }
+
+  if (error) {
+    return (
+      <section className="flex flex-col items-center justify-center p-5">
+        <div className="pt-20 font-heading text-heading-lg font-heading-bold text-red-500">
+          {error}
+        </div>
+      </section>
+    )
   }
 
   if (result) {
@@ -23,7 +60,7 @@ function ResultPage({ answers }: Props) {
           <h1 className="pt-20 font-heading text-heading-lg font-heading-bold text-spamBlue">
             You are just like: {result.name}!
           </h1>
-          <img src={`/images/hero_images/${result.image}`} alt="" />
+          <img src={`/images/hero_images/${result.image}`} alt={result.name} />
           <p className="m-4 mx-60 pb-20  font-body text-body-md">
             {result.info}
           </p>
@@ -31,5 +68,7 @@ function ResultPage({ answers }: Props) {
       </>
     )
   }
+
+  return null
 }
 export default ResultPage
