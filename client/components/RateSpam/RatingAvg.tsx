a376647350f8a907eb921d903f0ae3b2a73bc04c
Nikola Bodor <Nikola.Bodor@gmail.com>
2025-11-21 15:15:49 +1300

    feat: changed the stars for spam and colours

--- client/components/RateSpam/RatingAvg.tsx
+++ client/components/RateSpam/RatingAvg.tsx
@@ -1,6 +1,9 @@
 import { useAvgRatingById } from '../../hooks/useRatings'
+import { styled } from '@mui/material/styles'
 import Rating from '@mui/material/Rating'
 import Typography from '@mui/material/Typography'
+import PanoramaWideAngleSelectIcon from '@mui/icons-material/PanoramaWideAngleSelect'
+import PanoramaWideAngleOutlinedIcon from '@mui/icons-material/PanoramaWideAngleOutlined'
 
 export default function RatingAvg({ spamId }: { spamId: number }) {
   const { data, isError, isPending, error } = useAvgRatingById(spamId)
@@ -16,6 +19,14 @@ export default function RatingAvg({ spamId }: { spamId: number }) {
     return <p>uh-oh something went wrong</p>
   }
 
+  const StyledRating = styled(Rating)({
+    '& .MuiRating-iconFilled': {
+      color: '#ff6d75',
+    },
+    '& .MuiRating-iconHover': {
+      color: '#ff3d47',
+    },
+  })
 
   // function handleChange(event: any) {
   //   // TODO: Adding a Rating:
@@ -24,11 +35,14 @@ export default function RatingAvg({ spamId }: { spamId: number }) {
 
   return (
     <>
-      <Typography component="legend">Rating</Typography>
-      <Rating name="read-only" value={data} readOnly />
-      {/* TODO: Display a Rating: 
-        // the data using a customisable <Rating/> 
-        // from the MUI library */}
+      <Typography component="legend">Spam Rating</Typography>
+      <StyledRating
+        name="customized-color"
+        value={data}
+        icon={<PanoramaWideAngleSelectIcon fontSize="inherit" />}
+        emptyIcon={<PanoramaWideAngleOutlinedIcon fontSize="inherit" />}
+        readOnly
+      />
     </>
   )
 }
